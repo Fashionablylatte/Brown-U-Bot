1246e45ae6fbede9db6e3847b23275ad7741b90f
cheungs01 <44142677+cheungs01@users.noreply.github.com>
2021-01-23 23:06:33 -0500

    Comment for constants

--- utils/constants.py
+++ utils/constants.py
@@ -1,3 +1,5 @@
+# Endpoint for covid data api 
 COVID_ENDPT = 'https://api.covidtracking.com/v1/states/'
+
 CONFIG_FILEPATH = 'config.ini'
 KEYWORDS = {"covid", "coronavirus", "corona", "pandemic", "quarantine", "covid-19"}
